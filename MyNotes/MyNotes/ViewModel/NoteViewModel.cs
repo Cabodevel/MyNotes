b721f19cdec31960514ef682ae57b057d1847e95
Carlos Bolorino <carlosbolorino@socialicity.co.uk>
2019-10-28 16:13:34 +0000

    ViewModel base

--- MyNotes/MyNotes/ViewModel/NoteViewModel.cs
+++ MyNotes/MyNotes/ViewModel/NoteViewModel.cs
@@ -1,12 +1,20 @@
 ﻿using MyNotesCore.Common;
+using Xamarin.Forms;
 
 namespace MyNotes.ViewModel
 {
-    public class NoteViewModel
+    public class NoteViewModel : ViewModelBase
     {
-        public string Title { get; set; }
+        private string _Title;
+        public string Title 
+        { 
+            get => _Title;
+            set { _Title = value; RaisePropertyChanged(() => Title); }
+        }
         public string Text { get; set; }
         public string Color { get; set; }
         public PriorityEnum Priority { get; set; }
+        public Command CreateCommand { get; set; }
+        public Command UpdateCommand { get; set; }
     }
 }
---
+++ MyNotes/MyNotes/ViewModel/ViewModelBase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace MyNotes.ViewModel
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisePropertyChanged<T>(Expression<Func<T>> property) 
+        {
+            var name = GetMemberInfo(property).Name; 
+            OnPropertyChanged(name); 
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private MemberInfo GetMemberInfo(Expression expression)
+        {
+            MemberExpression operand;
+            LambdaExpression lambdaExpression = (LambdaExpression)expression;
+            if (lambdaExpression.Body as UnaryExpression != null)
+            {
+                UnaryExpression body = (UnaryExpression)lambdaExpression.Body;
+                operand = (MemberExpression)body.Operand;
+            }
+            else
+            {
+                operand = (MemberExpression)lambdaExpression.Body;
+            }
+            return operand.Member;
+        }
+    }
+}
